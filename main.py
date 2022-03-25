886af2d3fc9fba7930263e9ae1060e6efa3fbfd9
Ahmad <ahmadasadi1381.22@gmail.com>
2022-03-25 18:51:56 +0430

    Add Class

--- main.py
+++ main.py
@@ -1,3 +1,17 @@
+class SocialMedia:
+    def __init__(self, name):
+        self.name = name
+
+    def get_name(self):
+        return self.name
+
+class Instagram:
+    instance1 = SocialMedia("Instagram")
+
+class Twitter:
+    instance2 = SocialMedia("Twitter")
+
+
 if __name__ == '__main__':
     print()
     
